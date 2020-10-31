e1c7fcbb7109d064630d74d9f755f46511f633ed
nchapon <nchapon@gmail.com>
2020-10-31 22:18:21 +0100

    Finally list sdkman directory

--- sdkman.el
+++ sdkman.el
@@ -1,42 +1,16 @@
-;;; sdkman.el --- Emacs Lisp Java Switcher with SDKMan. -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2020 Nicolas CHAPON
-;;
-;; Author: Nicolas CHAPON <https://github.com/nchapon>
-;; Maintainer: Nicolas CHAPON <nchapon@gmail.com>
-;; Created: September 27, 2020
-;; Modified: September 28, 2020
-;; Version: 0.0.1
-;; Keywords:
-;; Homepage: https://github.com/nchapon/sdkman.el
-;; Package-Requires: ((emacs "27.1"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;;
-;;  Emacs Java Switcher with SDKMan.
-;;
-;;; Code:
+(defcustom JAVA_BASE "~/.sdkman/candidates/java"
 
-(defcustom sdkman-candidates-base-dir "~/.sdkman/candidates/java"
   "Java Candidates Directory"
   :group 'sdkman
   :type 'string
   :safe 'stringp)
 
 
-(setq sdkman-init-shell "~/.sdkman/bin/sdkman-init.sh")
-
-
-;;
-;; This function returns the list of installed
-;;
 (defun sdk--versions ()
   "Return the list of installed JDK."
   (seq-remove
    (lambda (a) (or (equal a ".") (equal a "..") (equal a "current") (equal a ".DS_Store")))
-   (directory-files sdkman-candidates-base-dir)))
+   (directory-files JAVA_BASE)))
 
 
 (defun sdk-use-java ()
@@ -49,24 +23,17 @@
                (lambda (e i) (list e i)) (sdk--versions))
               nil t "")))
     ;; switch java version
-    (setenv "JAVA_HOME" (expand-file-name (concat sdkman-candidates-base-dir "/" ver)))
-    (eshell/addpath (concat (getenv "JAVA_HOME") "/bin/java")))
-  ;; show version
+    (setenv "JAVA_HOME" (expand-file-name (concat JAVA_BASE "/" ver)))
+    (setenv "PATH" (concat (getenv "JAVA_HOME") "/bin/java")))
+
   (sdk-current))
 
-
-(defun sdk-current ()
-  "Display the current selected Java version."
+(defun sdk-current-java ()
   (interactive)
-  ;; displays current java version
-  (message (sdk-command "sdk current java")))
-
-
-(defun sdk-command (cmd)
-    "DOCSTRING"
-  (interactive)
-  (shell-command-to-string (concat "source " (expand-file-name sdkman-init-shell) " && " cmd)))
-
+  (or
+   (when (getenv "JAVA_HOME")
+     (message (shell-command-to-string (concat "realpath " (getenv "JAVA_HOME")))))
+   (message "JAVA_HOME not set")))
 
 
 (provide 'sdkman)
